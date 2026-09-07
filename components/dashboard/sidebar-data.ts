6d809ea36e11f360e80a36f3a0fa392a0bc73f21
SafnasKaldeen <safnaskm.20@uom.lk>
2026-09-07 13:50:11 +0530

    asd

--- components/dashboard/sidebar-data.ts
+++ components/dashboard/sidebar-data.ts
@@ -68,7 +68,6 @@ export const sidebarGroups: SidebarGroup[] = [
     items: [
       { type: 'view', label: 'Wishlist', view: 'wishlist' },
       { type: 'view', label: 'My Cart', view: 'cart' },
-      { type: 'view', label: 'Following', view: 'following' },
       { type: 'view', label: 'Recently Viewed', view: 'recentlyViewed' },
       { type: 'view', label: 'My Following', view: 'myFollowing' },
     ],
