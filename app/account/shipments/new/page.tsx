b434251856aaee57b0c204ba8d79819babb8352e
SafnasKaldeen <safnaskm.20@uom.lk>
2026-09-08 11:33:02 +0530

    asd

--- app/account/shipments/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import AddShipmentPage from '@/components/dashboard/AddShipmentPage'
-
-export default function NewShipmentPage() {
-  return <AddShipmentPage />
-}
--- app/account/shipments/page.tsx
+++
@@ -1,5 +0,0 @@
-import ShipmentOrdersPage from '@/components/dashboard/ShipmentOrdersPage'
-
-export default function ShipmentsPage() {
-  return <ShipmentOrdersPage />
-}
